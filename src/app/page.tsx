d2fcc03d9e2f5133c1f9ebe8c441386b726a60af
adrleo <adrleo247@gmail.com>
2025-04-09 16:38:06 +0700

    fix styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 import SwapInterface from "@/components/swap-interface";
 import Navbar from "@/components/navbar";
 
+export const metadata = {
+  title: "WOWOSwap",
+};
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-black p-4">
